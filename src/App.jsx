8b31881893bf223398578ee3d7f18c2954e764ac
Danny Marshall <dmarshall2019@gmail.com>
2026-02-23 16:11:24 -0600

    Create Footer component in Footer.jsx; implement Footer component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import './App.css'
 
 function App() {
 
   return (
     <div className='app'>
-      <Header storeName='Marshall Rose' headerMessage='
-        A new indie fashion house with something for everyone
-        Find and customize high quality garments guaranteed to stand out!'></Header>
+      <Header storeName='Marshall Rose' 
+        headerMessage='A new indie fashion house with something for everyone
+        Find and customize high quality garments guaranteed to stand out!'
+      ></Header>
+
+      <Footer storeName='Marshall Rose'
+        info='A new indie fashion house'
+        content='Thank you for shopping with us'
+      ></Footer>
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer({storeName, info, content}) {
+    return <div className='footer'>
+        <h1 className='footer-name'>{storeName}</h1>
+        <p className='footer-text'>{info}</p>
+        <p className='footer-text'>{content}</p>
+    </div>
+}
+
+export default Footer;
