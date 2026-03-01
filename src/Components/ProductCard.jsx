090ebbfbe6aa0ffe1ff8680e31742a958e242e4a
Danny Marshall <dmarshall2019@gmail.com>
2026-03-01 14:12:57 -0600

    Write documentation in README.md

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -6,6 +6,7 @@ function ProductCard({product}) {
         <div className='product-card'>
             <div className='img-placeholder'>
                 <h3>Placeholder for product image</h3>
+                {product.imageUrl ? <img src={product.imageUrl} crossOrigin="anonymous"/> : <p> No Image</p>}
             </div>
             <div className='product-info'>
                 <p className='product-card-txt'>{product.name}</p>
