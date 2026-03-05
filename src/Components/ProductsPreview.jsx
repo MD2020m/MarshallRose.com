27925ce6a8829c4ccc409390226340d8afe01398
Danny Marshall <dmarshall2019@gmail.com>
2026-03-05 14:13:55 -0600

    Implement featured products selection on ProductPreview component to be displayed on Home page

--- src/Components/ProductsPreview.jsx
+++ src/Components/ProductsPreview.jsx
@@ -1,8 +1,32 @@
-function ProductsPreview() {
+import ProductCard from "./ProductCard";
+import { Link } from 'react-router-dom';
+
+function ProductsPreview({ products }) {
+
+    let featuredProducts;
+    if (products.length > 5) {
+        featuredProducts = [
+            products[1],
+            products[2],
+            products[3],
+            products[4],
+            products[5]
+        ];
+    } else {
+        featuredProducts = products;
+    }
+
     return (
         <div className='products-preview'>
-            <h2 id='prev-products'>Products</h2>
-            <p>Placeholder</p>
+            <h2 id='prev-products'>Featured Products</h2>
+            <div className='featured-products-div'>
+                {featuredProducts.map(product => (<ProductCard product={product} />))}
+            </div>
+            <button className='nav-btn'>
+                <Link to='/products' className='nav-link'>
+                    Shop all products
+                </Link>
+            </button>
         </div>
     )
 }
